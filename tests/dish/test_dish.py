9b14e3651f53448f3b46cbc4d8a2d6c975fec7bc
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-09-12 15:36:17 -0300

    REQ 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,49 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
-# Req 2
+# Define test function
 def test_dish():
-    pass
+    pizza = "pizza"
+    feijoada = "feijoada"
+
+    dish_feijoada = Dish(feijoada, 22.00)
+    dish_pizza_sea = Dish(pizza, 32.90)
+    dish_pizza = Dish(pizza, 32.90)
+
+    assert dish_pizza_sea.name == pizza
+    assert repr(dish_pizza_sea) == f"Dish('{pizza}', R${32.90:.2f})"
+
+    assert dish_pizza_sea == dish_pizza
+    assert dish_pizza_sea != dish_feijoada
+
+    assert hash(dish_pizza_sea) == hash(dish_pizza)
+    assert hash(dish_pizza_sea) != hash(dish_feijoada)
+
+    with pytest.raises(ValueError):
+        Dish(pizza, 0)
+
+    with pytest.raises(TypeError):
+        Dish(pizza, "trinta e dois e noventa")
+
+    ingredient_octopus = Ingredient("polvo")
+    ingredient_shrimp = Ingredient("camarão")
+
+    dish_pizza_sea.add_ingredient_dependency(ingredient_octopus, 19)
+    dish_pizza_sea.add_ingredient_dependency(ingredient_shrimp, 35)
+
+    assert dish_pizza_sea.get_ingredients() == {
+        ingredient_octopus,
+        ingredient_shrimp,
+    }
+
+    assert dish_pizza_sea.get_restrictions() == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+
+if __name__ == "__main__":
+    test_dish()
