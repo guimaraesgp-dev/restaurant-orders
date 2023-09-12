c9eff2454f8bc075c93f1c61828a7f89d34a23ce
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-09-12 16:10:06 -0300

    REQ 4

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,19 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        menu = []
+
+        for dish in self.menu_data.dishes:
+            if self._is_dish_allowed(dish, restriction):
+                dish_info = {
+                    "dish_name": dish.name,
+                    "price": dish.price,
+                    "ingredients": dish.get_ingredients(),
+                    "restrictions": dish.get_restrictions(),
+                }
+                menu.append(dish_info)
+
+        return menu
+
+    def _is_dish_allowed(self, dish, restriction):
+        return not restriction or restriction not in dish.get_restrictions()
